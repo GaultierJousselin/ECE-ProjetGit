12742cdee347d7f0e3e64b29c233345bf4edb02d
Quentin MULLIEZ <33511073+Druwww@users.noreply.github.com>
2018-02-13 16:51:50 +0100

    j ai fait plein de trucs bizzare...

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 include <stdlib>
 
+class Projet {
+private:
+    int m_data;
+
+public:
+    Projet();
+    virtual ~Projet ();
+
+    void setData(int m){m_data = m;}
+    int getData(){return m_data;}
+};
+
 int main(int argc, char const *argv[]) {
   std::cout << "hello World";
-  std::cout << "\nBonjour le monde";
+  std::cout << "\nBonjour le monde\n";
+
+  Projet mdr;
+  mdr.setData(15);
+  std::cout << mdr.getData << "\n\n";
+
+
   return 0;
 }
